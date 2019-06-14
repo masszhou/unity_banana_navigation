99f4bc4b2474a2365e8ffd6f19c08b0c55d1a8df
Zhiliang Zhou <zhouzhiliang@gmail.com>
2019-06-14 11:06:08 +0200

    * saver bug fixed, saver must behind graph definition\n * reformat code

--- DQN_tf.py
+++ DQN_tf.py
@@ -37,9 +37,13 @@ class DeepQNetwork:
                  update_every=4,
                  batch_size=64,
                  memory_size=int(1e5),
-                 saved_path="./models/",
+                 model_save_path="./models/",
+                 log_save_path="./logs/",
                  output_graph=True):
 
+        # ------------------------------------------
+        # model parameters
+        # ------------------------------------------
         self.n_actions = n_actions
         self.n_features = n_features
         self.lr = learning_rate
@@ -56,42 +60,29 @@ class DeepQNetwork:
 
         # total learning step
         self.learn_step_counter = 0
+
+        # ------------------------------------------
+        # define network
+        # ------------------------------------------
+        # definition sequence in tf is important, so i put all definition together in __init__,
+        # in case of stupid mistakes
+        self.endpoints = {}
         self.global_step = tf.Variable(0, name='global_step', trainable=False)
 
-        # log and saver
-        self.saver = tf.train.Saver(max_to_keep=10)
-        self.saved_path = saved_path
-
-        # ------------------ all inputs ------------------------
         self.s = tf.placeholder(tf.float32, [None, self.n_features], name='s')  # input State
         self.s_next = tf.placeholder(tf.float32, [None, self.n_features], name='s_')  # input Next State
         self.r = tf.placeholder(tf.float32, [None, ], name='r')  # input Reward
         self.a = tf.placeholder(tf.int32, [None, ], name='a')  # input Action
-        self.endpoints = {}
-        self.build_network()
-
-        t_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='target_net')
-        e_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='eval_net')
-
-        # ToDo, soft-update with tau ?
-        with tf.variable_scope('hard_replacement'):
-            self.target_replace_op = [tf.assign(t, e) for t, e in zip(t_params, e_params)]
-
-        # no op definition after session started
-        self.sess = tf.Session()
-        self.sess.run(tf.global_variables_initializer())
-
-    def build_network(self):
         w_initializer = tf.random_normal_initializer(0., 0.3)
         b_initializer = tf.constant_initializer(0.1)
 
         with tf.variable_scope("eval_net") as scope:
-            eval_net = tf.keras.layers.Dense(64,
+            eval_net = tf.keras.layers.Dense(20,
                                              activation=tf.keras.activations.relu,
                                              kernel_initializer=w_initializer,
                                              bias_initializer=b_initializer,
                                              name="dense1")(self.s)
-            eval_net = tf.keras.layers.Dense(64,
+            eval_net = tf.keras.layers.Dense(20,
                                              activation=tf.keras.activations.relu,
                                              kernel_initializer=w_initializer,
                                              bias_initializer=b_initializer,
@@ -101,7 +92,7 @@ class DeepQNetwork:
                                              kernel_initializer=w_initializer,
                                              bias_initializer=b_initializer,
                                              name="Q_eval")(eval_net)
-            self.endpoints["Q_eval"] = eval_net # [n_batch, n_actions]
+            self.endpoints["Q_eval"] = eval_net  # [n_batch, n_actions]
             # note, Q_eval(shape=(?, 2)) has Q values with all actions
 
             a_indices = tf.stack([tf.range(tf.shape(self.a)[0], dtype=tf.int32), self.a], axis=1)
@@ -109,16 +100,16 @@ class DeepQNetwork:
             self.endpoints["Q_eval_wrt_a"] = tf.gather_nd(self.endpoints["Q_eval"], indices=a_indices)  # shape=(None, )
 
         with tf.variable_scope("target_net") as scope:
-            target_net = tf.keras.layers.Dense(64,
+            target_net = tf.keras.layers.Dense(20,
                                                activation=tf.keras.activations.relu,
                                                kernel_initializer=w_initializer,
                                                bias_initializer=b_initializer,
-                                               name="dense")(self.s_next)
-            target_net = tf.keras.layers.Dense(64,
+                                               name="dense1")(self.s_next)
+            target_net = tf.keras.layers.Dense(20,
                                                activation=tf.keras.activations.relu,
                                                kernel_initializer=w_initializer,
                                                bias_initializer=b_initializer,
-                                               name="dense")(target_net)
+                                               name="dense2")(target_net)
             target_net = tf.keras.layers.Dense(self.n_actions,
                                                activation=tf.keras.activations.relu,
                                                kernel_initializer=w_initializer,
@@ -135,7 +126,26 @@ class DeepQNetwork:
                                                              self.endpoints["Q_eval_wrt_a"], name='TD_error'))
         with tf.variable_scope('train'):
             # self.train_op = tf.train.RMSPropOptimizer(self.lr).minimize(self.loss)
-            self.train_op = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(self.loss, global_step=self.global_step)
+            self.train_op = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(self.loss,
+                                                                                   global_step=self.global_step)
+
+        t_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='target_net')
+        e_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='eval_net')
+
+        # ToDo, soft-update with tau ?
+        with tf.variable_scope('hard_replacement'):
+            self.target_replace_op = [tf.assign(t, e) for t, e in zip(t_params, e_params)]
+
+        # log and saver
+        # cao, saver must defined after network!!!
+        self.saver = tf.train.Saver(max_to_keep=1)
+        self.saved_path = model_save_path
+
+        # no op definition after session started
+        self.sess = tf.Session()
+        self.sess.run(tf.global_variables_initializer())
+        if output_graph is True:
+            file_writer = tf.summary.FileWriter(logdir=log_save_path, graph=self.sess.graph)
 
     def act(self, state, eps):
         s = np.expand_dims(state, axis=0)
@@ -180,14 +190,23 @@ class DeepQNetwork:
         _, loss, self.learn_step_counter = self.sess.run([self.train_op, self.loss, self.global_step], feed_dict=feed_dict)
 
     def save(self):
-        self.saver.save(self.sess, self.saved_path + 'dqn', global_step=self.global_step)
+        self.saver.save(self.sess, self.saved_path + 'dqn.ckpt', global_step=self.global_step)
 
     def restore(self):
         self.saver.restore(self.sess, tf.train.latest_checkpoint(self.saved_path))
+        self.learn_step_counter = self.sess.run(self.global_step)
+
+
+def debug_print(sess):
+    # default graph
+    vars = tf.trainable_variables()
+    vars_vals = sess.run(vars)
+    for var, val in zip(vars, vars_vals):
+        print(var.name)
+        print("shape: {}, sum: {}".format(val.shape, np.sum(val)))
 
 
 if __name__ == "__main__":
-    from tqdm import tqdm
     import gym
     import os
     os.environ['CUDA_VISIBLE_DEVICES'] = '0'  # or any '0,1'
@@ -207,10 +226,13 @@ if __name__ == "__main__":
     agent = DeepQNetwork(n_actions=env.action_space.n,
                          n_features=env.observation_space.shape[0],
                          learning_rate=0.01)
+    # debug_print(sess=agent.sess)
+    # agent.restore()
+    # print("----------")
+    # debug_print(sess=agent.sess)
 
     N_EPISODES = 100
     scores_log = []
-    pbar = tqdm(total=N_EPISODES)
 
     eps_start = 1.0
     eps_end = 0.01
@@ -223,7 +245,7 @@ if __name__ == "__main__":
         steps = 0
         while True:
             #env.render()
-            action = agent.choose_action(state, eps)
+            action = agent.act(state, eps)
             next_state, reward, done, info = env.step(action)
             # the smaller theta and closer to center the better
             x, x_dot, theta, theta_dot = next_state
@@ -244,13 +266,16 @@ if __name__ == "__main__":
             eps = max(eps_end, eps_decay * eps)
             if done:
                 break
-            if steps > 2000:
+            if steps > 500:
                 # sucessful episode
                 break
 
         scores_log.append(steps)
-        pbar.set_description('episode {}, epsilon {}'.format(i_episode, round(eps, 2)))
-        pbar.set_postfix(steps=steps)
-        pbar.update()
+        print("----------")
+        print("episode {}, epsilon {}, learn_steps {}, steps {}".format(i_episode,
+                                                                        round(eps, 2),
+                                                                        agent.learn_step_counter,
+                                                                        steps))
+        debug_print(sess=agent.sess)
 
-    pbar.close()
+    agent.save()
